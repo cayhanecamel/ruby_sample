618616af45caa9b7f8bc83570abca7dcb4f7ac9c
cayhanecamel <cayhane.camel@gmail.com>
2015-04-04 20:04:34 +0900

    add

---
+++ print_sample.rb
@@ -0,0 +1,11 @@
+print "hello world!" # 終わったあとに改行がない
+puts "hello world(puts)" # 改行がある
+p "hello world(p)" # データの形式がわかるように表示,改行もある
+p "hello world(p)" # データの形式がわかるように表示、改行もある
+=begin
+コメント
+コメント
+=end
+
+
+
