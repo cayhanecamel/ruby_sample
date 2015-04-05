ce9de215148acaf9cd0bdfa80ceaca22e4ed7a96
cayhanecamel <cayhane.camel@gmail.com>
2015-04-05 12:20:13 +0900

    add variable sample

---
+++ 002_variable_sample.rb
@@ -0,0 +1,16 @@
+# 変数: 全て小文字
+# 定数: 先頭が大文字だと定数扱いだが、全部大文字で書くのがデファクト
+
+msg = "hello world"
+p msg
+
+ADMIN_EMAIL = "hoge@gmail.com"
+
+p ADMIN_EMAIL
+
+#定数を後から変更しようとするとエラー
+#ADMIN_EMAIL = "hogehoge"
+
+
+
+
